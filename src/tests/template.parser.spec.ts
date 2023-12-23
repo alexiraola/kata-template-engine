fbb23acf0de4e754650b64788a49c9592a84da3b
Alex Iraola <alex.iraola@gmail.com>
2023-12-23 19:33:25 +0100

    Move to its own file

--- src/tests/template.parser.spec.ts
+++ src/tests/template.parser.spec.ts
@@ -1,16 +1,4 @@
-function parseParams(template: string) {
-  // regex to match strings contained in ${}, values are added to group 1.
-  const regex = /\${([a-zA-Z0-9]*)}/gm;
-  return unique(group1Matches(template.matchAll(regex)));
-}
-
-function group1Matches(matches: IterableIterator<RegExpMatchArray>) {
-  return Array.from(matches, (match) => match[1]);
-}
-
-function unique<T>(array: T[]) {
-  return [...new Set(array)];
-}
+import { parseParams } from '../core/template.parser';
 
 describe('Template parser', () => {
   it('returns an empty array for a template with no params', () => {
