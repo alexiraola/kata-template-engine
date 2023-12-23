5ebffa974bf84fbcdb66043075b305cb2016e98d
Alex Iraola <alex.iraola@gmail.com>
2023-12-23 23:16:31 +0100

    Parse params and replace args in template

--- src/core/template.ts
+++ src/core/template.ts
@@ -1,9 +1,17 @@
+import { TemplateParser } from "./template.parser";
+
 export interface TemplateParams {
   [index: string]: string
 }
 
 export class TemplateEngine {
+  parser = new TemplateParser();
+
   render(template: string, args: TemplateParams): string {
-    return template.replace('${param1}', args.param1);
+    const params = this.parser.parseParams(template);
+
+    return params.reduce((template, param) => {
+      return template.replace(`\$\{${param}}`, args[param]);
+    }, template);
   }
 }
--- src/tests/template.spec.ts
+++ src/tests/template.spec.ts
@@ -8,11 +8,17 @@ describe('Template engine', () => {
     expect(engine.render(template, {})).toBe(template);
   });
 
-  it('replaces provided arguments in the template', () => {
+  it('replaces provided argument in the template', () => {
     const template = 'example template with an ${param1}';
     const engine = new TemplateEngine();
 
     expect(engine.render(template, { param1: 'argument' })).toBe('example template with an argument');
   });
 
+  it('replaces provided arguments in the template', () => {
+    const template = 'example template with an ${param1} and ${param2}';
+    const engine = new TemplateEngine();
+
+    expect(engine.render(template, { param1: 'argument', param2: 'argument2' })).toBe('example template with an argument and argument2');
+  });
 });
